42e66f8dd7ee22a2358d2eb7ce1b3f901b9467cb
Natalie <natalie.r.crow@gmail.com>
2024-12-09 16:06:35 -0700

    adding teams route to server

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ global.TextDecoder = TextDecoder;
 
 const app = require("./config/serverConfig.js");
 const todosRoutes = require("./routes/todosRoutes.js");
+const teamsRoutes = require("./routes/teamsRoutes.js");
 
 const port = process.env.PORT || 5000;
 
 app.use('/', todosRoutes);
+app.use('/teams', teamsRoutes);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
