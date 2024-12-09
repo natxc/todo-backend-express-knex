f25fbce3ec61bbb732f14c09927ca3d3ab3ffa4f
Natalie <natalie.r.crow@gmail.com>
2024-12-09 15:48:52 -0700

    update users migration

--- server/migrations/20241209223859_create-users.js
+++ server/migrations/20241209223859_create-users.js
@@ -1,15 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments('user_id').primary();
+        table.string('name');
+        table.string('email');
+        table.string('password');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users');
 };
+
