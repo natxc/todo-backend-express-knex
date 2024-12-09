7ca56f9a93a28260ec4d12ef6df636a1f9d008d0
Natalie <natalie.r.crow@gmail.com>
2024-12-09 15:52:23 -0700

    comments migration

--- server/migrations/20241209223907_create-comments.js
+++ server/migrations/20241209223907_create-comments.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('comments', function (table) {
+        table.increments('comment_id').primary();
+        table.string('content');
+        table.integer('issue_id');
+        table.integer('author_id');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('comments');
 };
