5d804f9ebbc5e0eb0f6c2a279919051e144e8f27
Natalie <natalie.r.crow@gmail.com>
2024-12-09 15:57:30 -0700

    adding model for teams

---
+++ server/models/teamsQueries.js
@@ -0,0 +1,39 @@
+const knex = require("../config/connection.js");
+
+async function all() {
+    return knex('teams');
+}
+
+async function get(id) {
+    const results = await knex('teams').where({ id });
+    return results[0];
+}
+
+async function create(name) {
+    const results = await knex('teams').insert({ name }).returning('*');
+    return results[0];
+}
+
+async function update(id, properties) {
+    const results = await knex('teams').where({ id }).update({ ...properties }).returning('*');
+    return results[0];
+}
+
+// delete is a reserved keyword
+async function del(id) {
+    const results = await knex('teams').where({ id }).del().returning('*');
+    return results[0];
+}
+
+async function clear() {
+    return knex('teams').del().returning('*');
+}
+
+module.exports = {
+    all,
+    get,
+    create,
+    update,
+    delete: del,
+    clear
+}
