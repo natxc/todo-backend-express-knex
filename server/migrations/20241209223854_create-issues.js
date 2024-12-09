1d08f8c661813717d2d34bdea1625c3977ff04fb
Natalie <natalie.r.crow@gmail.com>
2024-12-09 15:47:37 -0700

    issues migration

--- server/migrations/20241209223854_create-issues.js
+++ server/migrations/20241209223854_create-issues.js
@@ -1,15 +1,20 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('issues', function (table) {
+        table.increments('issue_id').primary();
+        table.integer('project_id');
+        table.string('title');
+        table.string('description');
+        table.string('status');
+        table.string('priority');
+        table.integer('reported_by');
+        table.integer('assigned_to');
+        table.timestamp('due_date').defaultTo(knex.fn.now());
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('issues');
 };
