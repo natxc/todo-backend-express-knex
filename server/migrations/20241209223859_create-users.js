5546f378bb1dc41d327a26a25abff6e6119de078
Natalie <natalie.r.crow@gmail.com>
2024-12-10 14:23:25 -0700

    updating users for some auth

--- server/migrations/20241209223859_create-users.js
+++ server/migrations/20241209223859_create-users.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
     return knex.schema.createTable('users', function (table) {
         table.increments('user_id').primary();
-        table.string('name');
-        table.string('email');
-        table.string('password');
+        table.string('name').notNullable();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
@@ -13,4 +13,3 @@ exports.up = function (knex) {
 exports.down = function (knex) {
     return knex.schema.dropTable('users');
 };
-
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const usersController = require('../controllers/usersController.js');
+const { authenticateToken } = require('../middleware/authMiddleware.js');
 
 const router = express.Router();
 
+router.get('/:id', authenticateToken, usersController.getUser);
 router.get('/', usersController.getAllUsers);
-router.get('/:id', usersController.getUser);
 
 router.post('/', usersController.postUser);
 router.patch('/:id', usersController.patchUser);
 
 router.delete('/', usersController.deleteAllUsers);
 router.delete('/:id', usersController.deleteUser);
-
 
 module.exports = router;
