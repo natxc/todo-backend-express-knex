e91e727427b5e6c7664417d8d5ef11ffcf668ae7
Natalie <natalie.r.crow@gmail.com>
2024-12-11 15:29:29 -0700

    chore: organize app and add new routes too

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ProjectsProvider } from './context/ProjectsContext';
-import { IssuesProvider } from './context/IssuesContext';
-import { TeamsProvider } from './context/TeamsContext';
+
 import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import ProjectsPage from './pages/Projects/ProjectsPage';
-import ProjectDetailsPage from './pages/Projects/ProjectDetailsPage';
-import IssuesPage from './pages/Issues/IssuesPage';
-import IssueDetailsPage from './pages/Issues/IssueDetailsPage';
-import TeamsPage from './pages/Teams/TeamsPage';
-import TeamDetailsPage from './pages/Teams/TeamDetailsPage';
 import ApiDocsRedirect from './pages/ApiDocsRedirect';
 import Notes from './pages/Notes';
+
+import { ProjectsProvider } from './context/ProjectsContext';
+import ProjectsPage from './pages/Projects/ProjectsPage';
+import ProjectDetailsPage from './pages/Projects/ProjectDetailsPage';
+
+import { IssuesProvider } from './context/IssuesContext';
+import IssuesPage from './pages/Issues/IssuesPage';
+import IssueDetailsPage from './pages/Issues/IssueDetailsPage';
+import CreateIssuePage from './pages/Issues/CreateIssuesPage';
+import EditIssuePage from './pages/Issues/EditIssuePage';
+
+
+import { TeamsProvider } from './context/TeamsContext';
+import TeamsPage from './pages/Teams/TeamsPage';
+import TeamDetailsPage from './pages/Teams/TeamDetailsPage';
 import CreateTeamPage from './pages/Teams/CreateTeamPage';
 import EditTeamPage from './pages/Teams/EditTeamPage';
 
@@ -31,6 +38,8 @@ const App = () => {
                 <Route path="/projects/:id" element={<ProjectDetailsPage />} />
                 <Route path="/issues" element={<IssuesPage />} />
                 <Route path="/issues/:id" element={<IssueDetailsPage />} />
+                <Route path="/issues/new" element={<CreateIssuePage />} />
+                <Route path="/issues/:id/edit" element={<EditIssuePage />} />
                 <Route path="/teams" element={<TeamsPage />} />
                 <Route path="/teams/new" element={<CreateTeamPage />} />
                 <Route path="/teams/:id" element={<TeamDetailsPage />} />
