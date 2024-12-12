58bb6f8399c7ab14390c0e0ae9783e12bc6db5ea
Natalie <natalie.r.crow@gmail.com>
2024-12-12 14:25:21 -0700

    update video player

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import videoFile from '../assets/Locking_In.mp4';
+// import videoFile from '../assets/Locking_In.mp4';
 
 const VideoPlayer = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <video width="600" controls>
-            <source src={videoFile} type="video/mp4" />
+            {/* <source src={videoFile} type="video/mp4" /> */}
             Your browser does not support the video tag.
         </video>
         </div>
