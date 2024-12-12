af3b1f178b76cf4c51858f647b2f54c8e4d29cac
Natalie <natalie.r.crow@gmail.com>
2024-12-11 20:36:03 -0700

    cleanup

--- client/src/api/comments.js
+++
@@ -1,5 +0,0 @@
-export const fetchComments = async (issue_id) => {
-    const response = await fetch(`/issues/${issue_id}/comments`);
-    if (!response.ok) throw new Error('Failed to fetch comments');
-    return response.json();
-};
